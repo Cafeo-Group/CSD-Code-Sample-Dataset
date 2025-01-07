db376c5d41fc039bb664ad086fa6f44db3d83641
yellowisk <oheitoralmeida2013@gmail.com>
2025-01-06 22:02:14 -0300

    doc: document ecosystems notebook

--- models/ecosystem.py
+++ models/ecosystem.py
@@ -6,7 +6,7 @@ class Ecosystem:
     eco_name: str
     
     def __str__(self):
-        return f"{self.name} ({self.url})"
+        return f"{self.name}"
     
     def __repr__(self):
         return self.__str__()
